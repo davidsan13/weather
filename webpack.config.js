d7aaa3b76c8711f618a517acaa3ae3d5b32b3eec
davidsan13 <sandavid08@yahoo.com>
2023-08-17 15:49:25 -0700

    build

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Testing',
+      title: 'Weather',
     }),
     // new HtmlWebpackInjectPlugin({
     //   externals: [
